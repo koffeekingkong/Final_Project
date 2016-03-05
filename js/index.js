a734651487eae20b85c77a00822dc4d4b23791a3
koffeekingkong <koffeekingkong@gmail.com>
2016-03-05 14:50:09 +0800

    05 mar - Final site
    Fixed works page and am able to toggle between projects.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 $( document ).ready(function() {
 
+  $("nav figure").click(function(){
+    var projectid = $(this)[0].id;
+    // showing project id that is up till my -
+    projectid = projectid.substr(0, projectid.indexOf('-'));
+    console.log ("project ", projectid);
+    $(".projects > *").css("display","none");
+    $("#"+projectid).css("display","flex");
 
-}
+  })
+
+});
