6c9088c212c034c01ab305e594ee0a7a9ed1375e
koffeekingkong <koffeekingkong@gmail.com>
2016-03-07 01:03:10 +0800

    fixed scroll to top

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@ $( document ).ready(function() {
 
   $("section figure").click(function(){
     var projectid = $(this)[0].id;
-    // showing project id that is up till my -
+    // showing project id that is up till my hyphen
+    // on click, the projectid will be loaded
     projectid = projectid.substr(0, projectid.indexOf('-'));
     console.log ("project ", projectid);
     $(".projects > *").css("display","none");
+    $(".works").slideDown();
     $("#"+projectid).css("display","flex");
-
+    $('html, body').animate({scrollTop : 0},800);
+    		return false;
   })
 
 });
